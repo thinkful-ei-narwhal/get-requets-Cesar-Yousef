40c337b8d444ecc703fb4365697f28ffff866168
Cesar Morales <cesarlenin2@live.com>
2020-03-25 11:20:40 -0700

    try to add description left as comment

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 'use strict';
 
 function getDogImage(input) {
-  console.log(`https://dog.ceo/api/breeds/image/random/${input}`);
+  console.log(input);
   fetch(`https://dog.ceo/api/breeds/image/random/${input}`)
     .then(response => response.json())
     .then(responseJson => 
       displayResults(responseJson))
-    .catch(error => alert('Something went wrong. Try again later.'));
+    .catch(error => console.log('Something went wrong. Try again later',error));
 }
 
 
 function displayResults(responseJson) {
   console.log(responseJson);
-  let messageArr= []
-  responseJson.message.forEach(event=>{
-    messageArr.push(`<img src="${event}" class="results-img">`);
+
+  let imgArr= [];
+    responseJson.message.forEach(event=>{
+        imgArr.push(`<img src="${event}" class="results-img">`);
   });
-  messageArr= messageArr.join('');
-  $('.results').htm(`${messageArr}`);
+
+//   let statusArr= [];
+//   responseJson.status.forEach(event=>{
+//     console.log(event);
+//     statusArr.push(`<p>${event}</p>`);
+// });
+
+// let messageArr=statusArr.join('');
+
+//     let messageArr= [];
+//     for(i=0;i<statusArr.length;i++){
+//     messageArr.push(imgArr[i]);
+//     messageArr.push(statusArr[i]);
+// }
+    let messageArr= imgArr.join('');
+
+
+  $('.results').html(`${messageArr}`);
 
   //display the results section
   $('.results').removeClass('hidden');
