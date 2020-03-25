4443e1fe06bec60eaf094d485b8432f3249ceb55
Cesar Morales <cesarlenin2@live.com>
2020-03-25 10:10:58 -0700

    modify to output multiple dogs

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 'use strict';
 
-function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random')
+function getDogImage(input) {
+  console.log(`https://dog.ceo/api/breeds/image/random/${input}`);
+  fetch(`https://dog.ceo/api/breeds/image/random/${input}`)
     .then(response => response.json())
     .then(responseJson => 
       displayResults(responseJson))
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
+
 function displayResults(responseJson) {
   console.log(responseJson);
-  //replace the existing image with the new one
-  $('.results-img').replaceWith(
-    `<img src="${responseJson.message}" class="results-img">`
-  )
+  let messageArr= []
+  responseJson.message.forEach(event=>{
+    messageArr.push(`<img src="${event}" class="results-img">`);
+  });
+  messageArr= messageArr.join('');
+  $('.results').htm(`${messageArr}`);
+
   //display the results section
   $('.results').removeClass('hidden');
 }
@@ -21,7 +26,7 @@ function displayResults(responseJson) {
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    getDogImage();
+    getDogImage($('input').val());
   });
 }
 
